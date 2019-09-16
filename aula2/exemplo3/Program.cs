9901a24d715dbf0778e1a94f27677cc28aad7e57
FIC <FIC@info.local>
2019-09-16 14:15:36 -0300

    Adicionado terceira estrutura condicional

---
+++ aula2/exemplo3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deixamos declarada como falsa por padrão
+            bool entrada = false;
+            int idade;
+            string retorno;
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            /* Como já atribuímos anteriormente, mudamos o estado da variável para verdadeira
+            caso entre nessa condição */
+            if(idade > 17){
+                entrada = true;
+            }
+            
+            if(entrada == true){
+                retorno = "Entrada permitida";
+            }
+            else
+            {
+                retorno = "Entrada não permitida";
+            }
+            
+            Console.WriteLine(retorno);
+        }
+    }
+}
