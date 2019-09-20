8cbe2b402942150beaf17536a90e2ebea69dfe66
FIC <FIC@info.local>
2019-09-20 12:29:27 -0300

    Vetor

---
+++ aula5/exemplo1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] media = new double[10];
+
+            for(int i = 0; i <= 9; i++){
+                Console.Write("Digite uma média: ");
+                media[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(media[3]);
+
+            for(int i = 0; i <= media.Length - 1; i++){
+                Console.WriteLine("Média: " + media[i]);
+            }
+        }
+    }
+}
