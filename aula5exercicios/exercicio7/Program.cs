4f404eebaf4198adf2f83bdaebdef8b8d63bf642
FIC <FIC@info.local>
2019-09-23 09:32:08 -0300

    Exercicios

--- aula5exercicios/exercicio7/Program.cs
+++ aula5exercicios/exercicio7/Program.cs
@@ -9,12 +9,21 @@ namespace exercicio7
             // Faça um algoritmo que leia os valores de um vetor de 10 posições e que apresente o
             // maior valor digitado e em qual posição ele se encontra.
 
+            int maior = 0;
+            int posicao = 0;
+
             int[] num = new int[10];
             
             for(int i = 0; i<=9; i++){
                 Console.WriteLine("Digite um número: ");
                 num[i] = int.Parse(Console.ReadLine());
+
+                if(num[i] > maior){
+                    maior = num[i];
+                    posicao = i;
+                }
             }
+            Console.WriteLine($"Maior: {maior}, Posição: {posicao}");    
         }
     }
 }
