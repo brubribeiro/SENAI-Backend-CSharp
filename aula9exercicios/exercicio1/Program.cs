58781da65122138451ef1fb327ca3305a5e05ba0
FIC <FIC@info.local>
2019-09-27 15:27:17 -0300

    Exercicios Função

--- aula9exercicios/exercicio1/Program.cs
+++ aula9exercicios/exercicio1/Program.cs
@@ -10,26 +10,28 @@ namespace exercicio1
             // porcentagem, onde o intuito é atribuir um desconto sobre a porcentagem 
             // e retornar o valor aplicado o desconto
             try{
-            Console.Write("Digite o valor: ");
-            double valor = double.Parse(Console.ReadLine());
-            Console.Write("Digite o desconto %: ");
-            double porcentagem = double.Parse(Console.ReadLine().Replace("%",""));
+                Console.Write("Digite o valor: ");
+                double valor = double.Parse(Console.ReadLine());
+                Console.Write("Digite o desconto %: ");
+                double porcentagem = double.Parse(Console.ReadLine().Replace("%",""));
 
-            System.Console.WriteLine("O valor com desconto é de R$" + CalculaDesconto(valor, porcentagem));
+                System.Console.WriteLine("O valor com desconto é de R$" + CalculaDesconto(valor, porcentagem));
             }
             catch{
-                System.Console.WriteLine("Ops...deu erro!");
+                System.Console.WriteLine("Os dados devem ser númericos!");
             }
-
         }
+        /// <summary>
+        /// Função que aplica desconto em cima de um valor
+        /// </summary>
+        /// <param name="valor">Preço da mercadoria</param>
+        /// <param name="porcentagem">Percentual de desconto</param>
+        /// <returns>Valor do preço com o desconto aplicado</returns>
         static double CalculaDesconto( double valor, double porcentagem){
             double desconto = 0;
-            try{
+            
             desconto = valor - valor * (porcentagem/100);
-            }
-            catch{
-                System.Console.WriteLine("Ops...deu erro!");
-            }
+            
             return desconto;
         }
     }
