e2f52b68b7b76f3574a6599edfed587fbddf8be4
FIC <FIC@info.local>
2019-09-27 15:58:05 -0300

    Exercicios Função

---
+++ aula9exercicios/exercicio4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar um método do tipo double onde retorna o maior número de um vetor de tamanho indeterminado.
+            Console.Write("O maior número é: " + MaiorNum());
+        }
+        static double MaiorNum(double[] vetor){
+
+            double maior;
+
+            for(int i = 0; i < vetor.Length; i++){
+                if(vetor[0] < 0){
+                    maior = vetor[0];
+                }
+                else if(vetor[i] > maior){
+                    maior = vetor[i];
+                }
+            }
+            return maior;
+        }
+    }
+}
